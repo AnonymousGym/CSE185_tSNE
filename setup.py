4d5c80ee459d0782eb744adc4f95fbd10e77374f
AnonymousGym <97484485+AnonymousGym@users.noreply.github.com>
2023-05-22 13:20:36 -0700

    2.0

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name="CSE185-tSNE",
-    version="1.0",
+    version="2.0",
     description="first try",
     author="test",
-    py_modules=["CSE185_tSNE.getAdd"]
+    py_modules=["CSE185_tSNE.getAdd","CSE185_tSNE.try_run"]
 )
